0c6023438fd7b07941929242e47e6d326dadceac
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-06 12:05:57 +0900

    first commit

---
+++ MyHeizitukadai3App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyHeizitukadai3App
+//
+//  Created by VERTEX24 on 2019/08/06.
+//  Copyright © 2019 VERTEX24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
